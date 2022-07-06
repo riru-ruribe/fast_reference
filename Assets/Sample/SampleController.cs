bd2ba3d9b5829b7ca62079800100f5cca2d9bef9
riru_ruribe <riru_ruribe@yahoo.co.jp>
2022-07-06 10:55:26 +0900

    sample

---
+++ Assets/Sample/SampleController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleController : MonoBehaviour
+{
+    [SerializeField, FastReferenceMyself] Transform t;
+    [SerializeField, FastReference] Image image;
+    [SerializeField, FastReference("Btn")] Button button;
+    [SerializeField, FastReferenceFormat("Container_[0-9]")] SampleContainer[] containers;
+}
